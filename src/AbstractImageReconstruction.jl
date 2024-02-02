3e3ef0d0683f303c13ee10587b1ad9d704f912ff
nHackel <nHackel@users.noreply.github.com>
2024-02-02 17:15:16 +0100

    Remove dependency on RegLS

--- src/AbstractImageReconstruction.jl
+++ src/AbstractImageReconstruction.jl
@@ -3,7 +3,6 @@ module AbstractImageReconstruction
 using TOML
 using ThreadPools
 using Scratch
-using RegularizedLeastSquares
 using LRUCache
 
 import Base: put!, take!, fieldtypes, fieldtype, ismissing, propertynames, parent, hash
