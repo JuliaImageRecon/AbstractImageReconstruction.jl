78f8336eedb753073c6a27c7847f905d7d199398
Niklas Hackelberg <nHackel@users.noreply.github.com>
2024-12-11 17:37:59 +0100

    Update example_include_data.jl to hide output

--- docs/src/literate/example/example_include_data.jl
+++ docs/src/literate/example/example_include_data.jl
@@ -31,4 +31,4 @@ angles, shape, sinograms, images = isDataDefined ? (angles, shape, sinograms, im
     sinograms[:, :, :, i] = Array(RadonKA.radon(images[:, :, :, i], angles)) 
   end 
   return angles, shape, sinograms, images
-end
+end;
