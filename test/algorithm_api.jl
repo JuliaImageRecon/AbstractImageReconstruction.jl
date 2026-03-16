4dc145e15799a65cde9af5fc1a0495f19f4756f3
nHackel <nHackel@users.noreply.github.com>
2026-03-16 16:19:31 +0100

    Add @reconstruction macro tests

--- test/algorithm_api.jl
+++ test/algorithm_api.jl
@@ -18,3 +18,304 @@
   reco_2 = take!(algo)
   @test isapprox(reco_1, reco_2)
 end
+
+
+
+@testset "@reconstruction" begin
+
+  Base.@kwdef struct TestParameters <: AbstractTestParameters
+    value::Float64 = 1.0
+    iterations::Int64 = 100
+  end
+
+  Base.@kwdef struct AnotherParameters <: AbstractTestParameters
+    name::String = "test"
+  end
+
+  @testset "Basic algorithm definition" begin
+    @reconstruction mutable struct SimpleAlgorithm <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    # Check struct exists
+    @test @isdefined SimpleAlgorithm
+    
+    # Check constructor works
+    params = TestParameters(value=2.0)
+    algo = SimpleAlgorithm(params)
+    
+    # Check fields exist
+    @test hasfield(SimpleAlgorithm, :parameter)
+    @test hasfield(SimpleAlgorithm, :_channel)
+    @test algo.parameter === params
+  end
+
+  @testset "No supertype" begin
+    @reconstruction mutable struct NoBaseAlgorithm
+      @parameter parameter::TestParameters
+    end
+
+    @test @isdefined NoBaseAlgorithm
+    
+    # Check constructor works
+    params = TestParameters(value=2.0)
+    algo = NoBaseAlgorithm(params)
+    @test algo isa AbstractImageReconstructionAlgorithm
+  end
+
+  @testset "State fields with defaults and different types" begin
+    @reconstruction mutable struct AlgoWithDefaults <: AbstractTestBase
+      @parameter parameter::TestParameters
+      cache::Dict{String, Any} = Dict()
+      optional_state::Int = 42
+      name::String = "algorithm"
+    end
+    
+    params = TestParameters()
+    algo = AlgoWithDefaults(params)
+    
+    @test algo.cache == Dict()
+    @test algo.optional_state == 42
+    @test algo.name == "algorithm"
+  end
+
+  @testset "@init" begin
+    @reconstruction mutable struct AlgoWithInit <: AbstractTestBase
+      @parameter parameter::TestParameters
+      value::Union{Nothing, Int64} = nothing
+
+      @init function foo(algo::AlgoWithInit)
+        algo.value = 42
+      end
+    end
+    
+    params = TestParameters()
+    algo = AlgoWithInit(params)
+    @test algo.value == 42
+  end
+
+  @testset "Generic algorithm with type parameter" begin
+    @reconstruction struct GenericAlgorithm{P <: AbstractTestParameters} <: AbstractTestBase
+      @parameter parameter::P
+    end
+    
+    @test @isdefined GenericAlgorithm
+    
+    # Can construct with different parameter types
+    params1 = TestParameters(value=1.0)
+    algo1 = GenericAlgorithm(params1)
+    @test algo1.parameter === params1
+    
+    params2 = AnotherParameters(name="test")
+    algo2 = GenericAlgorithm(params2)
+    @test algo2.parameter === params2
+  end
+
+  @testset "put! and take! interface" begin
+    @reconstruction mutable struct InterfaceAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    # Define a simple process method
+    function process(algo::InterfaceAlgo, params::TestParameters, input)
+      return input * params.value
+    end
+    
+    params = TestParameters(value=2.0)
+    algo = InterfaceAlgo(params)
+    
+    # put! should work
+    @test_nowarn put!(algo, 5.0)
+    
+    # take! should retrieve the result
+    result = take!(algo)
+    @test result == 10.0
+  end
+
+  @testset "isready and wait" begin
+    @reconstruction mutable struct SyncAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    function process(algo::SyncAlgo, params::TestParameters, input)
+      return input + params.value
+    end
+    
+    params = TestParameters(value=1.0)
+    algo = SyncAlgo(params)
+    
+    @test !isready(algo)
+    
+    put!(algo, 5.0)
+    @test isready(algo)
+    
+    take!(algo)
+    @test !isready(algo)
+  end
+
+  @testset "lock and unlock" begin
+    @reconstruction mutable struct LockAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    function process(algo::LockAlgo, params::TestParameters, inputs...)
+      return inputs[1]
+    end
+    
+    params = TestParameters()
+    algo = LockAlgo(params)
+    
+    # lock/unlock should work
+    @test_nowarn lock(algo)
+    @test_nowarn unlock(algo)
+    
+    # lock with function
+    result = Ref(0)
+    lock(algo) do
+      result[] = 42
+    end
+    @test result[] == 42
+  end
+
+  @testset "parameter accessor" begin
+    @reconstruction mutable struct ParamAccessAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    params = TestParameters(value=3.14)
+    algo = ParamAccessAlgo(params)
+    
+    retrieved_params = parameter(algo)
+    @test retrieved_params === params
+    @test retrieved_params.value == 3.14
+  end
+
+  @testset "Channel FIFO behavior" begin
+    @reconstruction mutable struct FIFOAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    function process(algo::FIFOAlgo, params::TestParameters, input)
+      return input
+    end
+    
+    params = TestParameters()
+    algo = FIFOAlgo(params)
+    
+    # Queue multiple items
+    put!(algo, 1)
+    put!(algo, 2)
+    put!(algo, 3)
+    
+    # Should retrieve in FIFO order
+    @test take!(algo) == 1
+    @test take!(algo) == 2
+    @test take!(algo) == 3
+  end
+
+  @testset "Algorithm with many state fields" begin
+    @reconstruction mutable struct ComplexAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+      cache::Dict = Dict()
+      vector::Vector{Int} = Int[]
+      flag::Bool = false
+      count::Int = 0
+      name::String = "complex"
+    end
+    
+    params = TestParameters()
+    algo = ComplexAlgo(params)
+    
+    @test algo.cache == Dict()
+    @test algo.vector == Int[]
+    @test algo.flag == false
+    @test algo.count == 0
+    @test algo.name == "complex"
+  end
+
+  @testset "Process with multiple inputs" begin
+    @reconstruction struct MultiInputAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    function process(algo::MultiInputAlgo, params::TestParameters, x, y, z)
+      return x + y + z + params.value
+    end
+    
+    params = TestParameters(value=10.0)
+    algo = MultiInputAlgo(params)
+    
+    put!(algo, 1, 2, 3)
+    result = take!(algo)
+    @test result == 16.0
+  end
+
+  @testset "Error: process not defined" begin
+    @reconstruction struct NoProcessAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    params = TestParameters()
+    algo = NoProcessAlgo(params)
+    
+    # put! should error if process is not defined
+    @test_throws MethodError put!(algo, 5.0)
+  end
+
+  @testset "Algorithm fields are mutable" begin
+    @reconstruction mutable struct MutableAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+      state::Dict = Dict()
+    end
+    
+    params = TestParameters()
+    algo = MutableAlgo(params)
+    
+    # Should be able to mutate state
+    algo.state["key"] = "value"
+    @test algo.state["key"] == "value"
+    
+    # Should be able to update reference fields
+    algo.state = Dict("new" => "dict")
+    @test algo.state == Dict("new" => "dict")
+  end
+
+  @testset "reconstruct() with algorithm" begin
+    @reconstruction mutable struct ReconstructAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    function process(algo::ReconstructAlgo, params::TestParameters, inputs...)
+      return inputs[1] + params.value
+    end
+    
+    params = TestParameters(value=5.0)
+    algo = ReconstructAlgo(params)
+    
+    result = reconstruct(algo, 10.0)
+    @test result == 15.0
+  end
+
+  @testset "reconstruct() is thread-safe" begin
+    @reconstruction struct ThreadAlgo <: AbstractTestBase
+      @parameter parameter::TestParameters
+    end
+    
+    function process(algo::ThreadAlgo, params::TestParameters, inputs...)
+      return inputs[1] * params.value
+    end
+    
+    params = TestParameters(value=2.0)
+    algo = ThreadAlgo(params)
+    
+    # Multiple concurrent reconstructs should work
+    results = []
+    @sync for i in 1:10
+      @async push!(results, reconstruct(algo, Float64(i)))
+    end
+    
+    @test length(results) == 10
+    @test sort(results) == 2.0 .* (1:10)
+  end
+
+end
