3bea7bee25c829fe6ab32e675d67e6b06774c5fc
nHackel <nHackel@users.noreply.github.com>
2026-03-16 12:12:56 +0100

    Add macro to define algorithm boilerplate

--- docs/src/literate/example/2_direct.jl
+++ docs/src/literate/example/2_direct.jl
@@ -45,28 +45,6 @@ function AbstractImageReconstruction.process(algoT::Type{<:AbstractDirectRadonAl
 end
 
 # Now we can define the algorithm type itself. Algorithms are usually constructed with one argument passing in the user parameters:
-mutable struct DirectRadonAlgorithm{D <: DirectRadonParameters} <: AbstractDirectRadonAlgorithm
-  parameter::D
-  output::Channel{Any}
-  DirectRadonAlgorithm(parameter::D) where D = new{D}(parameter, Channel{Any}(Inf))
+@reconstruction struct DirectRadonAlgorithm{D <: DirectRadonParameters} <: AbstractDirectRadonAlgorithm
+  @parameter parameter::D
 end
-# And they implement a method to retrieve the used parameters:
-AbstractImageReconstruction.parameter(algo::DirectRadonAlgorithm) = algo.parameter
-
-# Algorithms are assumed to be stateful. To ensure thread safety, we need to implement the `lock` and `unlock` functions. We will use the `output` channel as a lock:
-Base.lock(algo::DirectRadonAlgorithm) = lock(algo.output)
-Base.unlock(algo::DirectRadonAlgorithm) = unlock(algo.output)
-
-# And implement the `put!` and `take!` functions, mimicking the behavior of a FIFO channel for reconstructions:
-Base.take!(algo::DirectRadonAlgorithm) = Base.take!(algo.output)
-function Base.put!(algo::DirectRadonAlgorithm, data::AbstractArray{T, 4}) where {T} 
-  lock(algo) do
-    put!(algo.output, process(algo, algo.parameter, data))
-  end
-end
-
-# The way the behaviour is implemented here, the algorithm does not buffer any inputs and instead blocks until the currenct reconstruction is done. Outputs are stored until they are retrieved.
-
-# With `wait` and `isready` we can check if the algorithm is currently processing data or if it is ready to accept new inputs:
-Base.wait(algo::DirectRadonAlgorithm) = wait(algo.output)
-Base.isready(algo::DirectRadonAlgorithm) = isready(algo.output)
--- src/AlgorithmInterface.jl
+++ src/AlgorithmInterface.jl
@@ -129,3 +129,244 @@ parameter(algo::AbstractImageReconstructionAlgorithm) = error("$(typeof(algo)) m
 Return the wrapped parameter. Can themselves be utility parameters again
 """
 parameter(param::AbstractUtilityReconstructionParameters) = error("$(typeof(param)) must implement `parameter`")
+
+
+# Macro constructor
+export @reconstruction
+"""
+    ReconstructionSpec
+
+Internal specification for algorithm definition. Can be created manually or via macro.
+"""
+struct ReconstructionSpec
+  name::Symbol
+  type_params::Vector
+  abstract_base::Symbol
+  parameter_type::Union{Symbol, Nothing}
+  parameter_name::Union{Symbol, Nothing}
+  state_fields::Vector{Tuple{Symbol, Symbol, Any}}  # (name, type, default)
+end
+
+function define_algorithm(spec::ReconstructionSpec)
+  # Build struct fields: parameter + state + infrastructure
+  struct_fields = [
+    :($(spec.parameter_name)::$(spec.parameter_type)),
+    [:($(field[1])::$(field[2])) for field in spec.state_fields]...,
+    :(_channel::Channel{Any}),
+  ]
+  
+  # Build struct definition
+  algo_name_expr = if isempty(spec.type_params)
+    :($(spec.name))
+  else
+    :($(spec.name){$(spec.type_params...)})
+  end
+  
+  struct_def = :(
+    mutable struct $(algo_name_expr) <: $(spec.abstract_base)
+      $(struct_fields...)
+    end
+  )
+  
+  # Build constructor
+  field_defaults = [field[3] for field in spec.state_fields]
+  
+  ctor_init = :(
+    algo = $(spec.name)(
+      parameter,
+      $(field_defaults...),
+      Channel{Any}(Inf))
+  )
+    
+  ctor = :(
+    function $(spec.name)(parameter)
+      $ctor_init
+      return algo
+    end
+  )
+  
+  # Build interface methods
+  interface_methods = [
+    _build_put_method(spec),
+    _build_take_method(spec),
+    _build_isready_method(spec),
+    _build_wait_method(spec),
+    _build_lock_method(spec),
+    _build_unlock_method(spec),
+    _build_parameter_accessor(spec),
+  ]
+  
+  # Combine all definitions
+  return quote
+    $struct_def
+    $ctor
+    $(interface_methods...)
+  end
+end
+
+function _build_put_method(spec::ReconstructionSpec)
+  algo_type = spec.name
+  param_name = spec.parameter_name
+
+  
+  put_body = quote
+    lock(algo) do
+      # Execute the algorithm
+      result = process(algo, algo.$param_name, inputs...)      
+      put!(algo._channel, result)
+    end
+  end
+  
+  return :(
+    function Base.put!(algo::$(algo_type), inputs...)
+      $put_body
+    end
+  )
+end
+
+
+function _build_take_method(spec::ReconstructionSpec)
+  algo_type = spec.name
+  
+  take_body = :(return Base.take!(algo._channel))
+  
+  return :(
+    function Base.take!(algo::$(algo_type))
+      $take_body
+    end
+  )
+end
+
+function _build_isready_method(spec::ReconstructionSpec)
+  algo_type = spec.name
+  return :(
+    Base.isready(algo::$(algo_type)) = isready(algo._channel)
+  )
+end
+
+function _build_wait_method(spec::ReconstructionSpec)
+  algo_type = spec.name
+  return :(
+    Base.wait(algo::$(algo_type)) = wait(algo._channel)
+  )
+end
+
+function _build_lock_method(spec::ReconstructionSpec)
+  algo_type = spec.name
+  return :(
+    Base.lock(algo::$(algo_type)) = lock(algo._channel)
+  )
+end
+
+function _build_unlock_method(spec::ReconstructionSpec)
+  algo_type = spec.name
+  return :(
+    Base.unlock(algo::$(algo_type)) = unlock(algo._channel)
+  )
+end
+
+function _build_parameter_accessor(spec::ReconstructionSpec)
+  algo_type = spec.name
+  param_name = spec.parameter_name
+  return :(
+    AbstractImageReconstruction.parameter(algo::$(algo_type)) = algo.$param_name
+  )
+end
+
+function parse_algorithm_spec(head::Expr, body::Expr)
+  # Check if there's an explicit base type
+  if Meta.isexpr(head, :<:)
+    algo_head = head.args[1]  # AlgoName{D <: ...}
+    abstract_base = head.args[2]
+  else
+    # No base type specified, use default
+    algo_head = head
+    abstract_base = :(AbstractImageReconstructionAlgorithm)
+  end
+  
+  # Extract algorithm name and type parameters
+  if Meta.isexpr(algo_head, :curly)
+    name = Symbol(algo_head.args[1])
+    type_params = algo_head.args[2:end]
+  else
+    name = Symbol(algo_head)
+    type_params = []
+  end
+  
+  # Parse body
+  parameter_type = nothing
+  parameter_name = nothing
+  state_fields = Tuple{Symbol, Symbol, Any}[]
+  
+  for item in body.args
+    if Meta.isexpr(item, :macrocall)
+      macro_name = item.args[1]
+      
+      if macro_name == Symbol("@parameter")
+        # @parameter parameter::D
+        param_spec = item.args[3]
+        parameter_name = Symbol(param_spec.args[1])
+        parameter_type = param_spec.args[2]
+      else
+        @warn "Unexpected macro call $macro_name"
+      end
+    
+    elseif Meta.isexpr(item, :(::))
+      # Field with type: state::Type or state::Type = default
+      field_name = Symbol(item.args[1])
+      
+      if Meta.isexpr(item.args[2], :(=))
+        # state::Type = default
+        field_type = item.args[2].args[1]
+        field_default = item.args[2].args[2]
+      else
+        # state::Type
+        field_type = item.args[2]
+        field_default = nothing
+      end
+      
+      push!(state_fields, (field_name, field_type, field_default))
+    end
+  end
+  
+  if parameter_type === nothing || parameter_name === nothing
+    error("@parameter required in @algorithm")
+  end
+  
+  return ReconstructionSpec(
+    name,
+    type_params,
+    abstract_base,
+    parameter_type,
+    parameter_name,
+    state_fields)
+end
+
+"""
+    @reconstruction struct AlgoName{P <: Params} <: AbstractBase
+      @parameter parameter::P
+      field::Type = default
+      # ...
+      @validate hook!(algo, inputs...)
+      @finalize hook!(algo, result)
+    end
+
+Define a stateful algorithm with boilerplate automatically generated.
+Supports both `struct` and `mutable struct`.
+"""
+macro reconstruction(expr)
+  # Check if this is a struct definition
+  if !Meta.isexpr(expr, :struct)
+    error("@reconstruction must be applied to a struct definition (struct or mutable struct)")
+  end
+  
+  is_mutable = expr.args[1]  # true for mutable struct, false for struct
+  algo_head = expr.args[2]    # AlgoName{P <: Params} <: AbstractBase
+  body_block = expr.args[3]   # The struct body as Expr(:block, ...)
+  
+  # Parse the spec using the existing parser
+  spec = parse_algorithm_spec(algo_head, body_block)
+
+  # Generate and return the algorithm definition
+  return esc(define_algorithm(spec))
+end
