35c7fe56c5255beb4bfa2c7f1774b23b2a2a4fd9
nHackel <nHackel@users.noreply.github.com>
2026-03-22 08:04:01 +0100

    Delete unmaintained misc. algorithms

--- src/AbstractImageReconstruction.jl
+++ src/AbstractImageReconstruction.jl
@@ -16,6 +16,5 @@ import Base: getindex, setindex!
 include("AlgorithmInterface.jl")
 include("StructTransforms.jl")
 include("RecoPlans/RecoPlans.jl")
-include("MiscAlgorithms/MiscAlgorithms.jl")
 
 end # module
--- src/MiscAlgorithms/MiscAlgorithms.jl
+++
@@ -1,1 +0,0 @@
-include("ThreadPinnedAlgorithm.jl")
